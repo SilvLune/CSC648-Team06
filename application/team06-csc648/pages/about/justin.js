d3289253f39739c5427e2740b4708aedbf1446fa
shinjsjustin <shinjsjustin@gmail.com>
2023-02-22 17:25:11 -0800

    Finishing touches on about page
    tbd

--- application/team06-csc648/pages/about/justin.js
+++ application/team06-csc648/pages/about/justin.js
@@ -1,15 +1,12 @@
 import Link from 'next/link';
 import styles from '../../styles/Home.module.css';
-const paragraph = 'As a group lead, my primary responsibilities revolve around communication and management.  I am here to lead my team to the finish line by organizing development flow, establishing a healthy relationship with the customer, and to pull my teammates together into one unified powerhouse.  '
 
 export default function Justin(){
     return (
-        <div className={styles.card}>
+        <Link className={styles.card} href='/about/justinDetails'>
             <img src='/portrates/Self_Portrate.PNG' alt="Justin Shin" width="250" height="300"></img>
             <h1>Justin Shin</h1>
-            <p>Email: jshin7@mail.sfsu.edu</p>
             <h3>Group Lead</h3>
-            <p>{paragraph}</p>
-        </div>
+        </Link>
     )
 }
