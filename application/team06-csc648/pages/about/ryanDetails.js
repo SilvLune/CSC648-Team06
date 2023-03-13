348dcec246a4737daefc8c70ab28b7f5b97de39a
ryanscott0515 <ryanscott0515@gmail.com>
2023-03-13 15:04:42 -0700

    Uppercase extension test on Ryan about

--- application/team06-csc648/pages/about/ryanDetails.js
+++ application/team06-csc648/pages/about/ryanDetails.js
@@ -8,7 +8,7 @@ export default function RyanDetails() {
         <div className={styles.main}>
             <Link className={styles.description} href='/aboutHome'>Back</Link>
             <h1 className={styles.title}>Ryan Scott</h1>
-            <img src='/portrates/Ryan_Portrait.jpg' alt="Ryan Scott" width="250" height="300"></img>
+            <img src='/portrates/Ryan_Portrait.JPG' alt="Ryan Scott" width="250" height="300"></img>
             <p classname={styles.description}>Email: rscott10@sfsu.edu</p>
             <p classname={styles.description}>{paragraph}</p>
         </div>
