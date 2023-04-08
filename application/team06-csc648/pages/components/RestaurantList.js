86b360ae5d7d58337cf693b79abb8e7320120f65
shinjsjustin <shinjsjustin@gmail.com>
2023-04-07 21:32:19 -0700

    EMERGENCY

--- application/team06-csc648/pages/components/RestaurantList.js
+++ application/team06-csc648/pages/components/RestaurantList.js
@@ -14,7 +14,7 @@ export default function RestaurantList(){
     return (
       <div>
         {restaurants.map((restaurant) => (
-          <div className={searchStyles.searchResult}>
+          <div className={searchStyles.searchResult} key={restaurant.restaurant_id}>
               <img src={`data:image/png;base64,${Buffer.from(restaurant.logo).toString('base64')}`} className={searchStyles.logo} alt={`${restaurant.name} logo`} />
               <h1 className={searchStyles.name}>{restaurant.name}</h1>
               <h2 className={searchStyles.time}>Expected delivery time: {restaurant.avg_delivery_time} minutes</h2>
