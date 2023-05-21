a81fa1c86ce0677c4822c643350b8b94eced6c6e
SilvLune <99846289+SilvLune@users.noreply.github.com>
2023-05-20 17:12:35 -0700

    navbar buttons reposition

--- application/team06-csc648/pages/components/navBar.js
+++ application/team06-csc648/pages/components/navBar.js
@@ -9,21 +9,18 @@ const NavBar = () => {
         <div className={styles.navBar}key="key1">
           <div className={styles.firstRow}>
             <p className={styles.title}>CSC648/848 Spring 2023 Team06</p>
-
           </div>
 
           <div className={styles.center}>
             <Link className={styles.logo} href='/'><h1>Gateway</h1></Link>
-
             <SearchBar/>
           </div>
           <div className={styles.buttons}>
-            <Link href='/login/customer'><button>Login</button></Link>
-          
-          
-            <Link href='/aboutHome'>About us</Link>
+            <Link href='/'>Home</Link>
+            <Link href='/login/customer'>SFSU user</Link>
             <Link href='/login/driver'>Driver</Link>
             <Link href='/login/restaurant'>Restaurant</Link>
+            <Link href='/aboutHome'>About us</Link>
           </div>
         </div>
     );
